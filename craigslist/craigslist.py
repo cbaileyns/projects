807bb9bd39530c2659947e6bd562c0c93272ef47
Chris Bailey <christopher.charles.bailey@gmail.com>
2015-02-24 20:17:26 -0500

    Update craigslist.py
    improved upon yesterday. now returns a list of data. still many bugs

--- craigslist/craigslist.py
+++ craigslist/craigslist.py
@@ -1,9 +1,9 @@
 class Craigslist():
     '''Craigslist class accepts a url that parses the list of urls contained in the url which was initially passes'''
-    def __init__(self, url, last=None):
+    def __init__(self, url):
         '''Holds the url'''
         self.url = url
-        self.last = last
+        self.last = None
     def getLinks(self):
         '''Returns list of ad id's which will be passed to a new class'''
         req = requests.get(self.url)
@@ -11,16 +11,40 @@ class Craigslist():
         urls = html.find_all('a', class_="hdrlnk")
         return [urls[i]['href'].split("/")[3] for i in range(len(urls))]
     def getData(self, newurl):
-        link = self.url[:-1] + "/" + newurl
+        link = self.url[:-1].split("/")
+        link.pop(3)
+        link = "/".join(link) + "/" + newurl
         req = requests.get(link)
         html = BeautifulSoup(req.text)
         text = html.find(id="postingbody").text
         initialpost = html.find(id="display-date").text
         price = html.find_all("span", class_="price")[0].text
-        info = html.find_all("span", class_="housing")[0].text.split(" - ")
-        bedrooms = info[0][-3:][0]
-        sqft = info[1][:4]
-        lat = html.find(id="map")["data-latitude"]
-        lon = html.find(id="map")["data-longitude"]
-        address = html.find_all("div", class_="mapaddress")[0].text
-        return price, bedrooms, sqdt, lat, lon, address, text, initiapost
+        try:
+            info = html.find_all("span", class_="housing")[0].text.split(" - ")
+        except:
+            info = ""
+        try:
+            bedrooms = info[0][-3:][0]
+        except:
+            bedrooms = ""
+        try:
+            sqft = info[1][:4]
+        except:
+            sqft = ""
+        try:
+            lat = html.find(id="map")["data-latitude"]
+        except TypeError:
+            lat = ""
+        try:
+            lon = html.find(id="map")["data-longitude"]
+        except TypeError:
+            lon = ""
+        try:
+            address = html.find_all("div", class_="mapaddress")[0].text
+        except IndexError:
+            address = ""
+        return link, price, bedrooms, sqft, lat, lon, address, text, initialpost
+    def scrapeData(self):
+        linklist = self.getLinks()
+        self.last = linklist[0]
+        return [self.getData(linklist[i]) for i in range(10)]
