d86a88f59a24da870113bd18341647ea1b6a2e14
Chris Bailey <christopher.charles.bailey@gmail.com>
2015-03-03 22:59:32 -0500

    Update lendingclub.py

--- lendingclub/lendingclub.py
+++ lendingclub/lendingclub.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class LendingClub():
     
@@ -9,4 +10,10 @@ class LendingClub():
     
     def clean_data(self):
         self.data = self.data.query('loan_status == "Fully Paid" or loan_status == "Charged Off"')
-        self.data.loan_status = self.data.loan_status.map({"Fully Paid":0, "Charged Off":1})
+        self.data.loan_status = self.data.loan_status.map({"Fully Paid":0, "Charged Off":1})  
+        self.data.int_rate = self.data.int_rate.apply(lambda x: np.nan if x in ['-'] else np.float(x[:-1]))
+        self.data.revol_util = self.data.revol_util.apply(lambda x: np.nan if x in ['-'] else np.float(x[:-1]))
+        self.data["closed_acct"] = self.data.total_acct - self.data.open_acct
+        self.data["funded"] = self.data.funded_amnt_inv / self.data.loan_amnt
+        self.y = self.data.loan_status
+        self.data["unemployed"] = self.data.emp_length == "n/a"
