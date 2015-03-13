6005f9f0614c8a963a6f4512cd029645486351e5
Chris Bailey <christopher.charles.bailey@gmail.com>
2015-03-12 22:46:39 -0400

    Update lendingclub.py

--- lendingclub/lendingclub.py
+++ lendingclub/lendingclub.py
@@ -4,6 +4,7 @@ import datetime
 from ggplot import *
 from logisticregression import Logistic
 from pandasql import *
+import datetime
 
 class LendingClub():
     
@@ -29,7 +30,7 @@ class LendingClub():
         self.y = self.data.loan_status
         self.data["unemployed"] = self.data.emp_length == "n/a"
         self.drop("funded_amnt")
-        self.drop("sub_grade")
+        self.data.sub_grade = self.data.sub_grade.apply(lambda x: x[1])
         self.drop("emp_title")
         self.drop("pymnt_plan")
         self.drop("url")
@@ -71,58 +72,25 @@ class LendingClub():
         self.data["good_renewable"] = self.data.grade.map({"A":1,"B":1,"C":1,"D":0,"E":0,"F":0,"G":0}) * self.data.purpose.apply(lambda x: 1 if x == "renewable_energy" else 0)
         self.data["small_biz"] = self.data.purpose.apply(lambda x: 1 if x == "small_business" else 0)
         self.data["wedding"] = self.data.purpose.apply(lambda x: 1 if x == "wedding" else 0)
-        
+        self.data["mntly_inc"] = self.data.annual_inc / 12
+        self.data.dti = self.data.dti / 100
+        self.data["mntly_pay"] = self.data.mntly_inc * self.data.dti
+        self.data.mntly_pay += self.data.installment
+        self.data["ndti"] = self.data.mntly_pay / self.data.mntly_inc
+        self.data["dti_incr"] = (self.data.ndti - self.data.dti)
+        self.data.dti_incr = self.data.dti_incr.apply(lambda x: 0 if x == inf else x)
+        self.data["dtindti"] = self.data.dti_incr/self.data.ndti
+        self.data["fico_decline"] = 850 - self.data.fico_range_high
+        self.data["fico_decline_py"] = self.data.fico_decline / self.data.age
+        self.data["la"] = self.data.loan_amnt.apply(lambda x: 1 if x > 14000 else 0)
+        self.data.term = self.data.term.apply(lambda x: 1 if x == " 60 months" else 0)
+        self.data["lowfunded"] = self.data.funded.apply(lambda x: 1 if x <0.9 else 0)
+        self.data["ca"] = self.data.state_addr == "CA"
+        self.data["fl"] = self.data.state_addr == "FL"
+        self.data["mrtg"] = self.data.home_ownership == "MORTGAGE"
+            
 lc = LendingClub("loandata.csv")
+lc.test_vars()
 lc.adjust_fico()
 lc.adjust_desc()
 lc.adjust_dates()
-lc.get_x(["fico_range_high","open_acc","funded","unemployed","int_rate","revol_util","dti"])
-lc.interact(lc.data.loan_amnt, lc.data.grade)
-lc.test_vars()
-
-l = Logistic(lc.x.values, lc.y)
-l.fit()
-
-l.eval()
-
-lc.plot("loan_amnt") #default the higher the larger the loan
-lc.plot_facet("loan_amnt")
-lc.plot_facet("revol_util")
-lc.plot("dti")
-lc.plot_facet("dti")
-
-lc.plot("age")
-lc.plot_facet("age")
-
-lc.plot("annual_inc")
-lc.plot_facet("annual_inc") #noticably more defaults when income is less than 60k
-lc.plot("high_early") #earn money faster -> less default chances
-lc.plot("inq_per_year")
-
-
-df = lc.data
-query = '''select a.purpose, a.grade, sum(a.loan_status) / count(a.purpose), count(a.grade) from df a group by a.purpose, a.grade'''
-f = sqldf(query, locals())
-pd.DataFrame.to_csv(f,"purpose.csv")
-
-query = '''select a.grade, sum(a.loan_status) / count(a.grade), count(a.grade) from df a group by a.grade'''
-g = sqldf(query, locals())
-pd.DataFrame.to_csv(g, "grade.csv")
-
-a = pd.crosstab([lc.data.grade,lc.data.inq_last_6mths],lc.data.loan_status)
-a[2] = a[1]/(a[0]+a[1])
-#p(D) increases across most grades as inquiries increase
-
-a = pd.cut(lc.data.mths_since_last_record,10)
-b = pd.DataFrame({"bin":a,"result":lc.data.loan_status,"grade":lc.data.grade})
-c = pd.crosstab(b.bin,b.result)
-c[2] = c[1]/c[0]
-
-lc.data["mntly_inc"] = lc.data.annual_inc / 12
-lc.data.dti = lc.data.dti / 100
-lc.data["mntly_pay"] = lc.data.mntly_inc * lc.data.dti
-lc.data.mntly_pay += lc.data.installment
-lc.data["ndti"] = lc.data.mntly_pay / lc.data.mntly_inc
-lc.data["dti_incr"] = (lc.data.ndti - lc.data.dti)
-lc.data.dti_incr = lc.data.dti_incr.apply(lambda x: 0 if x == inf else x)
-lc.data["dtindti"] = lc.data.dti_incr/lc.data.ndti
